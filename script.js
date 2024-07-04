d667962e3189edac2946aaa97b611b6e6c9ac8bd
Johan Salazar <johanmsalazarr@gmail.com>
2024-07-03 21:11:46 -0400

    agregando novedades

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const display = document.getElementById('display');
-const audio = new Audio("resources/click-sound.wav")
+const buttons = document.getElementsByClassName('operator-btn-blue');
+const audio = new Audio("resources/click-sound.wav");
+
+
+//const buttons = document.getElementsByTagName("button");
+//buttons.setAttribute('id', 'operator-btn-blue');
 
 function sound(){
     audio.playbackRate = 4;
@@ -18,5 +23,18 @@ function calculate(){
     }
     catch(error){
         display.value = "Error";
+    }
+}
+function changeColorToBlue(){
+    for (const boton of buttons) {
+        boton.classList.remove ("operator-btn-red");
+        boton.classList.remove ("operator-btn-yellow");
+        boton.classList.remove ("operator-btn-blue");
+        boton.classList.add ("operator-btn-blue");
+    }
+}
+function changeColorToRed(){
+    for (const boton of buttons) {
+        boton.classList.add ("operator-btn-red");
     }
 }
